2898bc8c639f0a0907943ab38d65ec9c1ec89f4c
Mirza <amirza.techlaunch.io>
2020-06-20 04:47:23 -0400

    Updates

---
+++ precache-manifest.ac9f7295d3a5754f6c49a301a1e56be9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80c101d1ac26cfc06b6ebdd56cf7678f",
+    "url": "/Ratings-App/index.html"
+  },
+  {
+    "revision": "90205ac4d5005639a8ba",
+    "url": "/Ratings-App/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "6f7a0f5f893f1c02b326",
+    "url": "/Ratings-App/static/css/main.0707f019.chunk.css"
+  },
+  {
+    "revision": "90205ac4d5005639a8ba",
+    "url": "/Ratings-App/static/js/2.a0419a8e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Ratings-App/static/js/2.a0419a8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f7a0f5f893f1c02b326",
+    "url": "/Ratings-App/static/js/main.e896b2c2.chunk.js"
+  },
+  {
+    "revision": "1d3dd51879211c69cacc",
+    "url": "/Ratings-App/static/js/runtime-main.5584f341.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c6d6abe63ba0b8d893b0b9fad7b6f472.js"
+  "/Ratings-App/precache-manifest.ac9f7295d3a5754f6c49a301a1e56be9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Ratings-App/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
