8b0f12d92c3c8f375acbfdce6d259a6473d73c16
Mirza <amirza.techlaunch.io>
2020-06-20 06:24:09 -0400

    removed nav

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import Review from "./components/Review";
-import Ratings from "./components/Ratings";
-import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <Router>
-      <React.Fragment>
-        <NavBar />
-        <Switch>
-          <Route exact path="/review" component={Review} />
-          <Route path="/ratings" component={Ratings} />
-        </Switch>
-      </React.Fragment>
-    </Router>
+    <Review />
   );
 }
 
