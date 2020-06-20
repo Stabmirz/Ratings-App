75a30b5e64f893ad72245008c7eff4a8634d8fec
Mirza <amirza.techlaunch.io>
2020-06-20 04:09:37 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import Review from "./components/Review";
+
+function App() {
+  return (
+    <div className="App">
+      <Review />
+    </div>
+  );
+}
+
+export default App;
