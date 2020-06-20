18f0904adf394064318f0312de6a2608e02a67f7
Mirza <amirza.techlaunch.io>
2020-06-20 04:12:24 -0400

    Updates

---
+++ precache-manifest.683f5bf43773f6a4755d979ad8e447c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5e77154738a06c6d07a6be02be743df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0f5179c34170c405b43",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "8379450b3fda4b99cefc",
+    "url": "/static/css/main.0707f019.chunk.css"
+  },
+  {
+    "revision": "d0f5179c34170c405b43",
+    "url": "/static/js/2.1a54660e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1a54660e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8379450b3fda4b99cefc",
+    "url": "/static/js/main.5374450e.chunk.js"
+  },
+  {
+    "revision": "13e4e53a9a23896b66f1",
+    "url": "/static/js/runtime-main.0a691a5c.js"
+  }
+]);
