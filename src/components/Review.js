b25403d6838db1c456138ba3a70bd4c42ed48128
Mirza <amirza.techlaunch.io>
2020-06-20 16:00:11 -0400

    updated ratings component

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Rating from "@material-ui/lab/Rating";
 import Ratings from "./Ratings";
+import Header from './Header';
 import "./Review.css";
 
 
@@ -30,6 +31,7 @@ class Review extends Component {
   handleChange = (event) => {
     const { name, value } = event.target;
     console.log(this.state);
+
     if(name === "rating") {
       this.setState({
         [name]: parseInt(event.target.value),
@@ -39,7 +41,7 @@ class Review extends Component {
         [name]: value,
       });
     }
-        this.handleValidation(event);
+      this.handleValidation(event);
   };
 
   handleValidation = (event) => {
@@ -111,14 +113,10 @@ class Review extends Component {
     const { errors } = this.state;
     return (
       <React.Fragment>
+        {/* If form valid and agree button is pressed redirect to ratings component otherwise render review component */}
         {!this.state.isAgreed &&
         <div>
-          <header className="header margin-medium">
-            <h1 className="margin-small">
-              1815 Rose Gold Chronograph Watch - Brown Croco Strap
-            </h1>
-            <h2 className="margin-small">Review</h2>
-          </header>
+          <Header title={"Review"}/>
           <div className="main-container">
             <form onSubmit={this.handleSubmit} ref={(ref) => (this.rref = ref)}>
               <div className="container">
